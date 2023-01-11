26121035361a78d64fb07b836772f51cf653cc04
JYB <evia_a@naver.com>
2023-01-11 18:55:07 +0900

    webpack dev server hot 설정 테스트 - DOM API로 접근하여 테스트

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 console.log('hello')
 
 import "../public/img/d.png"
+
+const body = document.body
+console.log(body)
+
+const root = document.getElementById('root');
+console.log(root);
+
+const hello = document.createElement('h1');
+console.log(hello);
+
+root.append(hello);
+
+hello.innerHTML = 'hello, jumper'
+
+document.body.style.backgroundRepeat = 'no-repeat'
+document.body.style.backgroundImage = "url('../public/img/d.png')"
+
+console.log(hello.style);
