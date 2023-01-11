78c7aec7f5efd9fc0fa223561e6614004c36f50f
JYB <evia_a@naver.com>
2023-01-11 18:59:25 +0900

    테스트용으로 작성한 코드 삭제

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 console.log('hello')
 
-import "../public/img/d.png"
+// import "../public/img/d.png"
 
-const body = document.body
-console.log(body)
 
-const root = document.getElementById('root');
-console.log(root);
-
-const hello = document.createElement('h1');
-console.log(hello);
-
-root.append(hello);
-
-hello.innerHTML = 'hello, jumper'
-
-document.body.style.backgroundRepeat = 'no-repeat'
-document.body.style.backgroundImage = "url('../public/img/d.png')"
-
-console.log(hello.style);
