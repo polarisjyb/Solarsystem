a08f6fd209796aacce91c75afe5be998ffcd66bd
JYB <evia_a@naver.com>
2023-01-18 00:46:10 +0900

    plane 과 cube 삭제

--- src/three.js
+++ src/three.js
@@ -40,48 +40,6 @@ const init = () => {
 
   scene.add(axes);
 
-  /* planeGeometry 생성 */
-  const planeGeometry = new THREE.PlaneGeometry(60, 20);
-  // plane - 배경으로 사용할 2차원 사각형.
-  // planeGeometry (width: float, height: float, widthSegments: integer, heightSegments: integer)
-
-  /*
-    width - x축 값에 따른 너비. 기본값은 1
-    height - y축 값에 따른 높이. 기본값은 1
-    widthSegments - 너비 세그먼트. x축을 따라 면을 분할한다. 기본값은 1
-    heightSegments - 높이 세그먼트. y축을 따라 면을 분할한다. 기본값은 1
-  */
-
-  const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
-  // planeGeometry가 어떻게 보일지 설정 ( 평면의 색상이나 투명도 등 )
-
-  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  // 형태, 재질을 합침, Mesh 메서드 사용
-
-  // plane은 기본값이 수직으로 서있기 떄문에 rotation 값 조정으로 수평으로 만든다.
-  // plane을 장면에 추가하기 전에 위치 지정을 해야한다.
-  plane.rotation.x = -0.5 * Math.PI;
-  plane.position.x = 15;
-  plane.position.y = 0;
-  plane.position.z = 9;
-
-  scene.add(plane);
-
-  /* cube 생성 */
-  const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-  // wireframe: true -> solid 오브젝트가 아닌, wireframe으로 렌더링 되도록 한다.
-
-  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-  // cube 위치 지정
-  cube.position.x = -4;
-  cube.position.y = 3;
-  cube.position.z = 0;
-
-  // cube scene에 추가 
-  scene.add(cube);
-
   /* Sphere 생성 */
   const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
   // SphereGeometry (반지름: float, 너비세그먼트: integer, 높이세그먼트: integer)
