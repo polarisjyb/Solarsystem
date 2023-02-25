64c1842727aba8ce8301a71c8e8928bdc5db7a6c
JYB <evia_a@naver.com>
2023-02-25 16:59:57 +0900

    scene, camera, renderer 설정 및 html root 요소에 자식 요소로 출력 설정

--- src/three.js
+++ src/three.js
@@ -1,3 +1,10 @@
 import './style.css'
 import * as THREE from 'three';
 
+// scene, camera, renderer 생성
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('root').appendChild(renderer.domElement);
+
