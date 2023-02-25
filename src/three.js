b440d5775085f5efa0effff2db8716b1f43249a2
JYB <evia_a@naver.com>
2023-02-25 17:01:23 +0900

    태양 생성

--- src/three.js
+++ src/three.js
@@ -8,3 +8,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('root').appendChild(renderer.domElement);
 
+// 태양 생성
+const sunGeometry = new THREE.SphereGeometry(1, 32, 32);
+const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+scene.add(sun);
