759061e53fcb5c18871109b810dc6e683e3a6ba0
JYB <evia_a@naver.com>
2023-02-28 23:54:00 +0900

    class 문법을 이용하여 행성 생성자 함수 모듈화 적용 중

---
+++ src/planet.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+
+// Planet 생성 (반지름, 태양으로부터의 거리, 색깔)
+class Planet {
+  constructor(radius, distanceFromSun, color, wireframe) {
+    const geometry = new THREE.SphereGeometry(radius, 32, 32);
+    const material = new THREE.MeshBasicMaterial({ color, wireframe });
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.position.set(distanceFromSun, 0, 0);
+  }
+
+  getMesh() {
+    return this.mesh;
+  }
+
+};
+
+export default Planet;
