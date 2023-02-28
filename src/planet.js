9e263c266d6230700068badb57f9795932b3a2f1
JYB <evia_a@naver.com>
2023-02-28 23:55:40 +0900

    행성 생성자 함수에 빠진 매개변수- wireframe 추가

--- src/planet.js
+++ src/planet.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-// Planet 생성 (반지름, 태양으로부터의 거리, 색깔)
+// Planet 생성 (반지름, 태양으로부터의 거리, 색깔, 와이어프레임: true or false)
 class Planet {
   constructor(radius, distanceFromSun, color, wireframe) {
     const geometry = new THREE.SphereGeometry(radius, 32, 32);
