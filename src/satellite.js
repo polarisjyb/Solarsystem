2e825fd17a5c25a49c37a15fa5a40231ab6e6204
JYB <evia_a@naver.com>
2023-03-02 23:54:43 +0900

    달을 추가하기 위해 satellite(위성) 모듈 생성

---
+++ src/satellite.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+class Satellite {
+  constructor(radius, distanceFromPlanet, color, rotationSpeed, revolutionSpeed) {
+    const geometry = new THREE.SphereGeometry(radius, 32, 32);
+    const material = new THREE.MeshBasicMaterial({ color});
+    this.mesh = new THREE.Mesh(geometry, material);
+
+    // 행성을 기준으로 위성의 초기 위치 설정
+    this.mesh.position.set(distanceFromPlanet, 0, 0);
+
+    // 위성의 자전 및 공전 속도 설정
+    this.rotationSpeed = rotationSpeed;
+    this.revolutionSpeed = revolutionSpeed;
+    
+    // 위성의 자전 각도 초기화 값 0
+    this.rotationAngle = 0;
+
+    // 생성되는 인스턴스의 distanceFromPlanet 초기화 
+    this.distanceFromPlanet = distanceFromPlanet;
+  }
+
+  getMesh() {
+    return this.mesh;
+  }
+
+  update() {
+    // 위성의 자전 업데이트
+    this.rotationAngle += this.rotationSpeed;
+    this.mesh.rotation.y = this.rotationAngle;
+
+    // 행성 주변의 위성 위치 업데이트
+    this.mesh.position.x = Math.cos(this.orbitAngle) * this.distanceFromPlanet;
+    this.mesh.position.z = Math.sin(this.orbitAngle) * this.distanceFromPlanet;
+  }
+};
