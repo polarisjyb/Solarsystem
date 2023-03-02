4f5a1e1b026f8d14955131eaf4090d73c22046ab
JYB <evia_a@naver.com>
2023-03-02 22:49:35 +0900

    planet 모듈에 자전과 공전 애니메이션 추가 ( console.log를 통해 행성의 실시간 변화하는 y축 좌표 확인 가능 )

--- src/planet.js
+++ src/planet.js
@@ -1,18 +1,36 @@
 import * as THREE from 'three';
 
-// Planet 생성 (반지름, 태양으로부터의 거리, 색깔, 와이어프레임: true or false)
+// Planet 생성
+// (반지름, 태양으로부터의 거리, 색깔, 와이어프레임, 자전 속도, 공전 속도)
 class Planet {
-  constructor(radius, distanceFromSun, color, wireframe) {
+  constructor(radius, distanceFromSun, color, wireframe, rotationSpeed, revolutionSpeed) {
     const geometry = new THREE.SphereGeometry(radius, 32, 32);
     const material = new THREE.MeshBasicMaterial({ color, wireframe });
     this.mesh = new THREE.Mesh(geometry, material);
     this.mesh.position.set(distanceFromSun, 0, 0);
+    
+    // 자전, 공전 속도
+    this.distanceFromSun = distanceFromSun;
+
+    this.rotationSpeed = rotationSpeed;
+    this.revolutionSpeed = revolutionSpeed;
+
+    this.orbitAngle = 0;
+    this.mesh.rotation.y = Math.PI / 2;
   }
 
   getMesh() {
     return this.mesh;
   }
 
+  update() {
+    this.orbitAngle += this.revolutionSpeed;
+    this.mesh.position.x = Math.cos(this.orbitAngle) * this.distanceFromSun;
+    this.mesh.position.z = Math.sin(this.orbitAngle) * this.distanceFromSun;
+    
+    this.mesh.rotation.y += this.rotationSpeed;
+    // console.log(this.mesh.rotation.y)
+  }
 };
 
 export default Planet;
