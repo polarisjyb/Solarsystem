128fa01777d4321360ba38bb781057591a352b55
JYB <evia_a@naver.com>
2023-03-03 00:32:21 +0900

    sun 모듈 주석 추가 및 자전 애니메이션 추가

--- src/sun.js
+++ src/sun.js
@@ -1,11 +1,15 @@
 import * as THREE from 'three';
 
-// Sun 생성 (반지름, 색깔, 와이어프레임: true or false)
+// Sun 생성 (반지름, 색깔, 와이어프레임, 자전 속도)
 class Sun {
-  constructor(radius, color, wireframe) {
+  constructor(radius, color, wireframe, rotationSpeed) {
     const geometry = new THREE.SphereGeometry(radius, 32, 32);
     const material = new THREE.MeshBasicMaterial({ color, wireframe });
     this.mesh = new THREE.Mesh(geometry, material);
+
+    // 태양의 자전 및 각도 초기화
+    this.rotationAngle = 0;
+    this.rotationSpeed = rotationSpeed;
   }
 
   getMesh() {
@@ -15,6 +19,15 @@ class Sun {
   setPosition(x, y, z) {
     this.mesh.position.set(x, y, z);
   }
-}
+
+  update() {
+
+    // 태양의 자전 업데이트
+    this.rotationAngle += this.rotationSpeed;
+    this.mesh.rotation.y = this.rotationAngle;
+    
+    // console.log(this.mesh.rotation.y)
+  }
+};
 
 export default Sun;
