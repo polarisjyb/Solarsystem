8d1a45a9d213c223717152bcb25678e1bf0d1392
JYB <evia_a@naver.com>
2023-03-10 00:44:08 +0900

    main.js 파일 내에 var 변수 키워드를 let 키워드로 변경

--- src/main.js
+++ src/main.js
@@ -84,31 +84,32 @@ along with JsTagSphere.  If not, see <http://www.gnu.org/licenses/>.
  *     at all) to 1 (whole container width and height).
  *     by default 0.025.
  */
+
 function Clouder(params) {
 
-  var self = this;
-  var w, h, lastX, lastY;
-  var rho = 0, theta = 0;
-  var timer = null;
-  var closest = null;
-  var containerTop;
-  var containerLeft;
-  var timing = [1];
-  var timingMax = 8;
+  let self = this;
+  let w, h, lastX, lastY;
+  let rho = 0, theta = 0;
+  let timer = null;
+  let closest = null;
+  let containerTop;
+  let containerLeft;
+  let timing = [1];
+  let timingMax = 8;
 
-  var container;
-  var callback;
-  var fontSize, fontShift;
-  var colorMax, colorMin, colorBgr;
-  var xScale = 0.9, yScale = 0.9;
-  var interval = 50;
-  var stepAngle = 0.08722;
-  var idleMotion = 0.2;
-  var nonSense = 0.025;
-  var opaque = 0.4;
+  let container;
+  let callback;
+  let fontSize, fontShift;
+  let colorMax, colorMin, colorBgr;
+  let xScale = 0.4, yScale = 0.8;
+  let interval = 25;
+  let stepAngle = 0.08;
+  let idleMotion = 0.2;
+  let nonSense = 0.025;
+  let opaque = 0.2;
   
   
-  var objs = [];
+  let objs = [];
   
   
   function init() {
@@ -164,15 +165,15 @@ function Clouder(params) {
       } // if
       
       rho = idleMotion;
-      setClosest(null);
+      
   } // onMouseLeave
   
   
   function setupElems(elems) {
       
       if (elems) {
-          for (var e in elems) {
-              var c = {};
+          for (let e in elems) {
+              let c = {};
               c.text = elems[e].text;
               c.id = elems[e].id;
               c.weight = elems[e].weight;
@@ -185,11 +186,11 @@ function Clouder(params) {
   
   
   function setupSpans() {
-      for (var i = 0; i < container.children.length; i++) {
-          var span = container.children[i];
+      for (let i = 0; i < container.children.length; i++) {
+          let span = container.children[i];
           span.style.position = "absolute";
           span.style.cursor = "pointer";
-          var c = {};
+          let c = {};
           c.span = span;
           c.width = 0;
           c.height = 0;
@@ -200,17 +201,17 @@ function Clouder(params) {
   
   function adjustElems() {
       
-      for (var i in objs) {
-          var dx = 0, dy = 0, dz = 0;
-          var o = objs[i];
-          for (var j in objs) {
+      for (let i in objs) {
+          let dx = 0, dy = 0, dz = 0;
+          let o = objs[i];
+          for (let j in objs) {
               if (i == j) {
                   continue;
               } // if
-              var diffX = o.x - objs[j].x;
-              var diffY = o.y - objs[j].y;
-              var diffZ = o.z - objs[j].z;
-              var r = Math.sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ);
+              let diffX = o.x - objs[j].x;
+              let diffY = o.y - objs[j].y;
+              let diffZ = o.z - objs[j].z;
+              let r = Math.sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ);
               dx += 0.05 / (r * r) * diffX / r;
               dy += 0.05 / (r * r) * diffY / r;
               dz += 0.05 / (r * r) * diffZ / r;
@@ -219,7 +220,7 @@ function Clouder(params) {
           o.x += dx;
           o.y += dy;
           o.z += dz;
-          var dist = Math.sqrt(o.x * o.x + o.y * o.y + o.z * o.z);
+          let dist = Math.sqrt(o.x * o.x + o.y * o.y + o.z * o.z);
           o.x /= dist;
           o.y /= dist;
           o.z /= dist;
@@ -243,7 +244,7 @@ function Clouder(params) {
   } // setPos
   
   function draw() {
-      var filters = (typeof(document.body.filters) == "object");
+      let filters = (typeof(document.body.filters) == "object");
 
       process(function(o) {
           
@@ -255,8 +256,8 @@ function Clouder(params) {
               o.span.style.fontWeight = "bold";
               o.span.style.position = "absolute";
               o.span.style.cursor = "pointer";
-              var c = 1;
-              for (var i in colorMax) {
+              let c = 1;
+              for (let i in colorMax) {
                   c = c * 256 + Math.floor((colorMax[i] - colorMin[i]) * o.weight + colorMin[i]);
               } // for
               o.span.style.color = "#" + c.toString(16).substr(1);
@@ -264,7 +265,7 @@ function Clouder(params) {
               o.span.descriptor = o;
           } // if
           
-          var size = fontSize + o.z * fontShift;
+          let size = fontSize + o.z * fontShift;
           o.factor = size / fontSize;
           if (o.width == 0) {
               o.width = asPixels(o.span.clientWidth / o.factor);
@@ -276,11 +277,11 @@ function Clouder(params) {
           o.screenY = h * (o.y * yScale + 1) / 2;
           o.span.style.top = asPixels(o.screenY - parseInt(o.height) * o.factor / 2);
           o.span.style.zIndex = o.z >= 0 ? 10 : 5;
-          var opa = (Math.sin(o.z * Math.PI / 2) / 2 + 0.5) * (1 - opaque) + opaque;
+          let opa = (Math.sin(o.z * Math.PI / 2) / 2 + 0.5) * (1 - opaque) + opaque;
           if (!filters) {
               o.span.style.opacity = opa;
           } else {
-              var f = o.span.filters["DXImageTransform.Microsoft.Alpha"];
+              let f = o.span.filters[""];
               if (f) {
                   f.opacity = opa * 100;
               } else {
@@ -294,13 +295,13 @@ function Clouder(params) {
   
   function findClosest(ex, ey) {
 
-      var bestDist = w + h;
-      var best = null;
-      for (var i in objs) {
-          var o = objs[i];
-          var dx = ex - o.screenX;
-          var dy = ey - o.screenY;
-          var dist = Math.sqrt(dx * dx + dy * dy) / o.factor;
+      let bestDist = w + h;
+      let best = null;
+      for (let i in objs) {
+          let o = objs[i];
+          let dx = ex - o.screenX;
+          let dy = ey - o.screenY;
+          let dist = Math.sqrt(dx * dx + dy * dy) / o.factor;
           if (dist < bestDist) {
               bestDist = dist;
               best = o;
@@ -330,9 +331,9 @@ function Clouder(params) {
   
   
   function onTimer() {
-      var t0 = new Date().getTime();
+      let t0 = new Date().getTime();
       
-      var move = function(o) {
+      let move = function(o) {
           spin(o, -theta);
           step(o, rho * stepAngle);
           spin(o, theta);
@@ -352,16 +353,16 @@ function Clouder(params) {
   
   
   function spin(o, angle) {
-      var x = o.x;
-      var y = o.y;
+      let x = o.x;
+      let y = o.y;
       o.x = x * Math.cos(angle) - y * Math.sin(angle);
       o.y = x * Math.sin(angle) + y * Math.cos(angle);
   } // spin
       
       
   function step(o, angle) {
-      var x = o.x;
-      var z = o.z;
+      let x = o.x;
+      let z = o.z;
       o.x = x * Math.cos(angle) - z * Math.sin(angle);
       o.z = x * Math.sin(angle) + z * Math.cos(angle);
   } // step
@@ -378,14 +379,14 @@ function Clouder(params) {
   
   
   function process(func) {
-      for (var i in objs) {
+      for (let i in objs) {
           func(objs[i]);
       } // for
   } // process
       
   
   function parseColor(text) {
-      var hex = parseInt(text.substr(1), 16);
+      let hex = parseInt(text.substr(1), 16);
       return [Math.floor(hex / 65536), Math.floor((hex / 256) % 256), Math.floor(hex % 256)];
   } // parseColor
   
@@ -405,10 +406,10 @@ function Clouder(params) {
       } // if
       
       callback = p.callback ? p.callback : function(text) { alert(text); };
-      fontSize = p.fontSize ? p.fontSize : 32;
+      fontSize = p.fontSize ? p.fontSize : 48;
       fontShift = typeof(p.fontShift) != "undefined" ? p.fontShift : fontSize / 2;
-      colorMax = p.colorMax ? parseColor(p.colorMax) : parseColor("#000000");
-      colorMin = p.colorMin ? parseColor(p.colorMin) : parseColor("#C0C0C0");
+      colorMax = p.colorMax ? parseColor(p.colorMax) : parseColor("#00000");
+      colorMin = p.colorMin ? parseColor(p.colorMin) : parseColor("#c0c0c0");
       colorBgr = p.colorBgr ? p.colorBgr : null;
       interval = typeof(p.interval) != "undefined" ? p.interval : interval;
       stepAngle = typeof(p.stepAngle) != "undefined" ? p.stepAngle : stepAngle;
@@ -424,8 +425,8 @@ function Clouder(params) {
   
   
   self.getRenderingTime = function() {
-      var sum = 0;
-      for (var i in timing) {
+      let sum = 0;
+      for (let i in timing) {
           sum += timing[i];
       } // for
       return sum / timing.length;
